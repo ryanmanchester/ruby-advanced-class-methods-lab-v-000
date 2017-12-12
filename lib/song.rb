de5734e7c59a2df021cd7d6776dc3eb2e2078795
Ryan Manchester <ryanm17@gmail.com>
2017-12-12 02:01:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,6 +46,8 @@ class Song
 
   def self.new_from_filename(filename)
     file_parts = filename.split("-")
+    artist_name = file_parts[0]
+    song_name = file_parts[1].gsub(".mp3", " ")
 binding.pry
   end
 
